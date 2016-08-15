5ece93ce6402789f13e373975d1bac6aac4197d3
Luis <lcrojano@gmail.com>
2016-08-15 13:26:01 -0500

    creacion de servicios

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,5 +1,16 @@
 angular.module('app.controllers', [])
 
-.controller('SessionController', function($scope, $http, Test){
+.controller('SessionController', function($scope, $http, sessions){
+	$scope.questions = {};
+	sessions.getSessions(1, "token").then(function(data) {
+            //$scope.answers = data;
+            if (!$.isEmptyObject(data) && data !== null && typeof(data) != "undefined") {
+                $scope.questions = data;
+                active();
+            }else{
+                $scope.questions= {};
+            }
+            $log.debug("recibido en sessions controller: " ,$scope.questions);      
+    });
 	
 });
--- public/js/services.js
+++ public/js/services.js
@@ -1,4 +1,4 @@
-var url = "http://localhost:8888/pmp/public/";
+/*var url = "http://localhost:8888/pmp/public/";
 
 angular.module('app.services', [])
 
@@ -6,4 +6,58 @@ angular.module('app.services', [])
 	return{
 		
 	}
-});
+});*/
+
+  angular
+        .module('app.services',[])
+        .factory('sessions', sessions);
+
+    /** @ngInject */
+    function sessions($log, $http, Restangular) {
+        var apiHost = 'http://localhost/pmp_hugo/PMP/public/api/sessions/';
+        var service = {
+            apiHost: apiHost,
+            getAnswers: getAnswers,
+            setAnswers: setAnswers,
+            getAllAnswers: getAllAnswers
+        };
+
+        return service;
+
+
+        function getSessions(id,token) {
+            var new_answers = (JSON.parse(JSON.stringify(answers_structs[type])));
+            $log.info("obteniendo datos de la session" + id + " del usuario:" + token);
+            return $http.get(apiHost +"/" + id + "/questions")
+                .then(getAnswersComplete)
+                .catch(getAnswersFailed);
+
+            function getAnswersComplete(response) {
+                $log.info("devolviendo :", response);
+                if (response.status == 200) {
+                    if (typeof(response.data.answers) != "undefined" && response.data.answers != null) {
+                        return getInSessions(new_answers, response.data.answers);
+                    } else {
+                        return new_answers; //si no se ha realizado el insturmento
+                    }
+                }
+
+                return new_answers;
+
+            }
+
+            function getAnswersFailed(error) {
+                $log.error('XHR Failed for getAnswers.\n' + angular.toJson(error.data, true));
+                return new_answers;
+            }
+        }
+
+       
+
+
+
+
+
+
+    }
+ 
